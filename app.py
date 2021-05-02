c3b6871d37878c58b060bff0ceb02e5637801d33
Charlesyim <yanchicharles@gmail.com>
2021-05-02 14:04:26 +0800

    77

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask,render_template,request,make_response,send_from_directory,jsonify
+from flask import Flask,render_template,request
 from models import *
 from exts import db
-
+import pyecharts.options as opts
+from pyecharts.charts import Line
 from flask_paginate import Pagination,get_page_parameter
+
+
 app = Flask(__name__)
 
 HOSTNAME = '127.0.0.1'
@@ -26,174 +29,344 @@ def index():
 
 @app.route('/qs/')
 def qs_rank():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     qs = None
+    total = None
     if t:
         for i in range(2016,2022):
             if t == i:
-                qs = Qs_rank.query.filter_by(time=t).all()
-    return render_template('qs_rank.html',qs=qs,t=t)
+                qs = Qs_rank.query.filter_by(time=t).slice(start, end)
+                total = Qs_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('qs_rank.html',qs=qs,t=t,pagination=pagination)
 
 
 @app.route('/qs/subject')
 def qs_subject():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     s = request.args.get('s',type=str,default='science')
     qs_sub = None
+    total = None
     if s == 'science':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    qs_sub = Qs_science_rank.query.filter_by(time=t).all()
+                    qs_sub = Qs_science_rank.query.filter_by(time=t).slice(start, end)
+                    total = Qs_science_rank.query.filter_by(time=t).count()
     elif s == 'accounting':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    qs_sub = Qs_accounting_rank.query.filter_by(time=t).all()
+                    qs_sub = Qs_accounting_rank.query.filter_by(time=t).slice(start, end)
+                    total = Qs_science_rank.query.filter_by(time=t).count()
 
     elif s == 'enginnering':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    qs_sub = Qs_engineering_rank.query.filter_by(time=t).all()
+                    qs_sub = Qs_engineering_rank.query.filter_by(time=t).slice(start, end)
+                    total = Qs_science_rank.query.filter_by(time=t).count()
 
     elif s == 'technology':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    qs_sub = Qs_technology_rank.query.filter_by(time=t).all()
-
-    return render_template('qs_rank.html',qs_sub=qs_sub,t=t,s=s)
+                    qs_sub = Qs_technology_rank.query.filter_by(time=t).slice(start, end)
+                    total = Qs_science_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('qs_rank.html',qs_sub=qs_sub,t=t,s=s,pagination=pagination)
 
 
 
 @app.route('/the/')
 def the_rank():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     the = None
+    total = None
     if t:
         for i in range(2016,2022):
             if t == i:
-                the = The_rank.query.filter_by(time=t).all()
-    return render_template('the_rank.html',the=the,t=t)
+                the = The_rank.query.filter_by(time=t).slice(start,end)
+                total = The_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('the_rank.html',the=the,t=t,pagination=pagination)
 
 
 @app.route('/the/subject')
 def the_subject():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     s = request.args.get('s',type=str,default='science')
     the_sub = None
+    total = None
     if s == 'science':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    the_sub = The_science_rank.query.filter_by(time=t).all()
+                    the_sub = The_science_rank.query.filter_by(time=t).slice(start,end)
+                    total = The_science_rank.query.filter_by(time=t).count()
     elif s == 'accounting':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    the_sub = The_accounting_rank.query.filter_by(time=t).all()
+                    the_sub = The_accounting_rank.query.filter_by(time=t).slice(start,end)
+                    total = The_science_rank.query.filter_by(time=t).count()
 
     elif s == 'enginnering':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    the_sub = The_engineering_rank.query.filter_by(time=t).all()
+                    the_sub = The_engineering_rank.query.filter_by(time=t).slice(start,end)
+                    total = The_science_rank.query.filter_by(time=t).count()
 
     elif s == 'technology':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    the_sub = The_technology_rank.query.filter_by(time=t).all()
-
-    return render_template('the_rank.html',the_sub=the_sub,t=t,s=s)
+                    the_sub = The_technology_rank.query.filter_by(time=t).slice(start,end)
+                    total = The_science_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('the_rank.html',the_sub=the_sub,t=t,s=s,pagination=pagination)
 
 
 
 @app.route('/us_news/')
 def us_news_rank():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     us = None
+    total = None
     if t:
         for i in range(2016,2022):
             if t == i:
-                us = Us_rank.query.filter_by(time=t).all()
-    return render_template('us_news_rank.html',us=us,t=t)
+                us = Us_rank.query.filter_by(time=t).slice(start,end)
+                total = Us_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('us_news_rank.html',us=us,t=t,pagination=pagination)
 
 
 @app.route('/us_news/subject')
 def us_subject():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     s = request.args.get('s',type=str,default='science')
     us_sub = None
+    total = None
     if s == 'science':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    us_sub = Us_science_rank.query.filter_by(time=t).all()
+                    us_sub = Us_science_rank.query.filter_by(time=t).slice(start,end)
+                    total = Us_science_rank.query.filter_by(time=t).count()
     elif s == 'accounting':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    us_sub = Us_accounting_rank.query.filter_by(time=t).all()
+                    us_sub = Us_accounting_rank.query.filter_by(time=t).slice(start,end)
+                    total = Us_science_rank.query.filter_by(time=t).count()
 
     elif s == 'enginnering':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    us_sub = Us_engineering_rank.query.filter_by(time=t).all()
+                    us_sub = Us_engineering_rank.query.filter_by(time=t).slice(start,end)
+                    total = Us_science_rank.query.filter_by(time=t).count()
 
     elif s == 'technology':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    us_sub = Us_technology_rank.query.filter_by(time=t).all()
-
-    return render_template('us_news_rank.html',us_sub=us_sub,t=t,s=s)
+                    us_sub = Us_technology_rank.query.filter_by(time=t).slice(start,end)
+                    total = Us_science_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('us_news_rank.html',us_sub=us_sub,t=t,s=s,pagination=pagination)
 
 
 
 
 @app.route('/arwu/')
 def arwu_rank():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     arwu = None
+    total = None
     if t:
         for i in range(2016,2022):
             if t == i:
-                arwu = Arwu_rank.query.filter_by(time=t).all()
-    return render_template('arwu_rank.html',arwu=arwu,t=t)
+                arwu = Arwu_rank.query.filter_by(time=t).slice(start,end)
+                total = Arwu_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('arwu_rank.html',arwu=arwu,t=t,pagination=pagination)
 
 
 @app.route('/arwu/subject')
 def arwu_subject():
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    start = (page - 1) * 10
+    end = start + 10
     t = request.args.get('t',type=int,default=2021)
     s = request.args.get('s',type=str,default='science')
     arwu_sub = None
+    total = None
     if s == 'science':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    arwu_sub = Arwu_science_rank.query.filter_by(time=t).all()
+                    arwu_sub = Arwu_science_rank.query.filter_by(time=t).slice(start,end)
+                    total = Arwu_science_rank.query.filter_by(time=t).count()
     elif s == 'accounting':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    arwu_sub = Arwu_accounting_rank.query.filter_by(time=t).all()
+                    arwu_sub = Arwu_accounting_rank.query.filter_by(time=t).slice(start,end)
+                    total = Arwu_science_rank.query.filter_by(time=t).count()
 
     elif s == 'enginnering':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    arwu_sub = Arwu_engineering_rank.query.filter_by(time=t).all()
+                    arwu_sub = Arwu_engineering_rank.query.filter_by(time=t).slice(start,end)
+                    total = Arwu_science_rank.query.filter_by(time=t).count()
 
     elif s == 'technology':
         if t:
             for i in range(2016,2022):
                 if t == i:
-                    arwu_sub = Arwu_technology_rank.query.filter_by(time=t).all()
+                    arwu_sub = Arwu_technology_rank.query.filter_by(time=t).slice(start,end)
+                    total = Arwu_science_rank.query.filter_by(time=t).count()
+    pagination = Pagination(bs_version=4, page=page, total=total, outer_window=0, inner_window=2)
+    return render_template('arwu_rank.html',arwu_sub=arwu_sub,t=t,s=s,pagination=pagination)
 
-    return render_template('arwu_rank.html',arwu_sub=arwu_sub,t=t,s=s)
+
+@app.route('/qs/trend/')
+def qs_trend():
+    items = db.session.query(Qs_rank.chinese_name).order_by(Qs_rank.sea).all()
+    data1 = []
+    for item in items:
+        data1.append(item[0])
+    data2 = set(data1)
+    colleges = list(data2)
+    result = None
+    c = request.args.get('college',type=str,default='')
+
+    return render_template('qs_trend.html',colleges=colleges,name=c)
+
+
+def line_base(name,result):
+    c = (
+        Line()
+
+            .add_xaxis([str(item[1]) for item in result ])
+            .add_yaxis(f"{name}", [str(item[0]) for item in result])
+            .set_global_opts(title_opts=opts.TitleOpts(title="排名趋势"))
+
+    )
+    return c
+
+
+@app.route("/qs/lineChart/")
+def get_line_qs_chart():
+    college = request.args.get('college')
+    result = db.session.query(Qs_rank.sea, Qs_rank.time).filter_by(chinese_name=f'{college}').order_by(Qs_rank.time).all()
+
+    c = line_base(college,result)
+    return c.dump_options_with_quotes()
+
+
+@app.route('/the/trend/')
+def the_trend():
+    items = db.session.query(The_rank.chinese_name).order_by(The_rank.sea).all()
+    data1 = []
+    for item in items:
+        data1.append(item[0])
+    data2 = set(data1)
+    colleges = list(data2)
+    result = None
+    c = request.args.get('college',type=str,default='')
+
+    return render_template('the_trend.html',colleges=colleges,name=c)
+
+
+@app.route("/the/lineChart/")
+def get_line_the_chart():
+    college = request.args.get('college')
+    result = db.session.query(The_rank.sea, The_rank.time).filter_by(chinese_name=f'{college}').order_by(The_rank.time).all()
+
+    c = line_base(college,result)
+    return c.dump_options_with_quotes()
+
+
+@app.route('/us_news/trend/')
+def us_trend():
+    items = db.session.query(Us_rank.chinese_name).order_by(Us_rank.sea).all()
+    data1 = []
+    for item in items:
+        data1.append(item[0])
+    data2 = set(data1)
+    colleges = list(data2)
+    result = None
+    c = request.args.get('college',type=str,default='')
+
+    return render_template('us_news_trend.html',colleges=colleges,name=c)
+
+@app.route("/us_news/lineChart/")
+def get_line_us_chart():
+    college = request.args.get('college')
+    result = db.session.query(Us_rank.sea, Us_rank.time).filter_by(chinese_name=f'{college}').order_by(Us_rank.time).all()
+
+    c = line_base(college,result)
+    return c.dump_options_with_quotes()
+
+
+
+@app.route('/arwu/trend/')
+def arwu_trend():
+    items = db.session.query(Arwu_rank.chinese_name).order_by(Arwu_rank.sea).all()
+    data1 = []
+    for item in items:
+        data1.append(item[0])
+    data2 = set(data1)
+    colleges = list(data2)
+    result = None
+    c = request.args.get('college',type=str,default='')
+
+    return render_template('arwu_trend.html',colleges=colleges,name=c)
+
+@app.route("/arwu/lineChart/")
+def get_line_arwu_chart():
+    college = request.args.get('college')
+    result = db.session.query(Arwu_rank.sea, Arwu_rank.time).filter_by(chinese_name=f'{college}').order_by(Arwu_rank.time).all()
+
+    c = line_base(college,result)
+    return c.dump_options_with_quotes()
+
+
+@app.route('/search/')
+def search():
+    college = request.args.get('kw')
+
+    if Qs_rank.query.filter_by(chinese_name=f'{college}').all() or The_rank.query.filter_by(chinese_name=f'{college}').all() or Us_rank.query.filter_by(chinese_name=f'{college}').all() or Arwu_rank.query.filter_by(chinese_name=f'{college}').all():
+        return render_template('search.html',college=college)
+    else:
+        return render_template('search.html', massage='暂无学校信息')
+
 
 
 
