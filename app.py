7fb1f96156168f157fa46d0bfcea52f8040c2585
Charlesyim <yanchicharles@gmail.com>
2021-05-02 10:18:34 +0800

    77

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ def qs_subject():
     return render_template('qs_rank.html',qs_sub=qs_sub,t=t,s=s)
 
 
+
 @app.route('/the/')
 def the_rank():
     t = request.args.get('t',type=int,default=2021)
@@ -77,13 +78,124 @@ def the_rank():
     return render_template('the_rank.html',the=the,t=t)
 
 
+@app.route('/the/subject')
+def the_subject():
+    t = request.args.get('t',type=int,default=2021)
+    s = request.args.get('s',type=str,default='science')
+    the_sub = None
+    if s == 'science':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    the_sub = The_science_rank.query.filter_by(time=t).all()
+    elif s == 'accounting':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    the_sub = The_accounting_rank.query.filter_by(time=t).all()
+
+    elif s == 'enginnering':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    the_sub = The_engineering_rank.query.filter_by(time=t).all()
+
+    elif s == 'technology':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    the_sub = The_technology_rank.query.filter_by(time=t).all()
+
+    return render_template('the_rank.html',the_sub=the_sub,t=t,s=s)
+
+
+
 @app.route('/us_news/')
 def us_news_rank():
-    return render_template('us_news_rank.html')
+    t = request.args.get('t',type=int,default=2021)
+    us = None
+    if t:
+        for i in range(2016,2022):
+            if t == i:
+                us = Us_rank.query.filter_by(time=t).all()
+    return render_template('us_news_rank.html',us=us,t=t)
+
+
+@app.route('/us_news/subject')
+def us_subject():
+    t = request.args.get('t',type=int,default=2021)
+    s = request.args.get('s',type=str,default='science')
+    us_sub = None
+    if s == 'science':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    us_sub = Us_science_rank.query.filter_by(time=t).all()
+    elif s == 'accounting':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    us_sub = Us_accounting_rank.query.filter_by(time=t).all()
+
+    elif s == 'enginnering':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    us_sub = Us_engineering_rank.query.filter_by(time=t).all()
+
+    elif s == 'technology':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    us_sub = Us_technology_rank.query.filter_by(time=t).all()
+
+    return render_template('us_news_rank.html',us_sub=us_sub,t=t,s=s)
+
+
+
 
 @app.route('/arwu/')
 def arwu_rank():
-    return render_template('arwu_rank.html')
+    t = request.args.get('t',type=int,default=2021)
+    arwu = None
+    if t:
+        for i in range(2016,2022):
+            if t == i:
+                arwu = Arwu_rank.query.filter_by(time=t).all()
+    return render_template('arwu_rank.html',arwu=arwu,t=t)
+
+
+@app.route('/arwu/subject')
+def arwu_subject():
+    t = request.args.get('t',type=int,default=2021)
+    s = request.args.get('s',type=str,default='science')
+    arwu_sub = None
+    if s == 'science':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    arwu_sub = Arwu_science_rank.query.filter_by(time=t).all()
+    elif s == 'accounting':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    arwu_sub = Arwu_accounting_rank.query.filter_by(time=t).all()
+
+    elif s == 'enginnering':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    arwu_sub = Arwu_engineering_rank.query.filter_by(time=t).all()
+
+    elif s == 'technology':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    arwu_sub = Arwu_technology_rank.query.filter_by(time=t).all()
+
+    return render_template('arwu_rank.html',arwu_sub=arwu_sub,t=t,s=s)
+
+
 
 if __name__ == '__main__':
     app.run()
