3c3b782ebd567128c643c066e8bc445e52542932
Charlesyim <yanchicharles@gmail.com>
2021-05-02 09:45:19 +0800

    77

--- app.py
+++ app.py
@@ -26,12 +26,56 @@ def index():
 
 @app.route('/qs/')
 def qs_rank():
-    qs = Qs_rank.query.filter_by(time=2021).all()
-    return render_template('qs_rank.html',qs=qs)
+    t = request.args.get('t',type=int,default=2021)
+    qs = None
+    if t:
+        for i in range(2016,2022):
+            if t == i:
+                qs = Qs_rank.query.filter_by(time=t).all()
+    return render_template('qs_rank.html',qs=qs,t=t)
+
+
+@app.route('/qs/subject')
+def qs_subject():
+    t = request.args.get('t',type=int,default=2021)
+    s = request.args.get('s',type=str,default='science')
+    qs_sub = None
+    if s == 'science':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    qs_sub = Qs_science_rank.query.filter_by(time=t).all()
+    elif s == 'accounting':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    qs_sub = Qs_accounting_rank.query.filter_by(time=t).all()
+
+    elif s == 'enginnering':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    qs_sub = Qs_engineering_rank.query.filter_by(time=t).all()
+
+    elif s == 'technology':
+        if t:
+            for i in range(2016,2022):
+                if t == i:
+                    qs_sub = Qs_technology_rank.query.filter_by(time=t).all()
+
+    return render_template('qs_rank.html',qs_sub=qs_sub,t=t,s=s)
+
 
 @app.route('/the/')
 def the_rank():
-    return render_template('the_rank.html')
+    t = request.args.get('t',type=int,default=2021)
+    the = None
+    if t:
+        for i in range(2016,2022):
+            if t == i:
+                the = The_rank.query.filter_by(time=t).all()
+    return render_template('the_rank.html',the=the,t=t)
+
 
 @app.route('/us_news/')
 def us_news_rank():
